1777a1d449ef5df8e9f257879430ef3f4ddf5ebc
Ido Adler <games@ido.wtf>
2022-08-31 22:44:44 +0300

    added touch ui

--- levels/movement.gd
+++ levels/movement.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
 export var SPEED = 250
+export var exceleration = 3
+export var decexeleration = 3
 export(float) var animation_speed = 1.0
 
 onready var player = $"AnimationPlayer"
@@ -15,26 +17,29 @@ func _process(delta):
 		player.stop(true)
 		player.play("FadeToPink", 0, animation_speed)
 
-	if Input.is_action_just_released("ui_up"):
-		velocity.y = 0
-	if Input.is_action_just_released("ui_down"):
-		velocity.y = 0
-	if Input.is_action_just_released("ui_left"):
-		velocity.x = 0
-	if Input.is_action_just_released("ui_right"):
-		velocity.x = 0
-
 	var motion = Vector2(0,0)
 	if Input.is_action_pressed("ui_up"):
-		motion += Vector2(0,-1)
-	if Input.is_action_pressed("ui_down"):
-		motion += Vector2(0,1)
+		motion += Vector2(0,-1) * exceleration * delta
+	elif Input.is_action_pressed("ui_down"):
+		motion += Vector2(0,1) * exceleration * delta
+	elif velocity.y > decexeleration * delta:
+		motion.y -= decexeleration * delta
+	elif velocity.y < -decexeleration * delta:
+		motion.y += decexeleration * delta
+	else:
+		velocity.y = 0
 	if Input.is_action_pressed("ui_left"):
-		motion += Vector2(-1,0)
-	if Input.is_action_pressed("ui_right"):
-		motion += Vector2(1,0)
+		motion += Vector2(-1,0) * exceleration * delta
+	elif Input.is_action_pressed("ui_right"):
+		motion += Vector2(1,0) * exceleration * delta
+	elif velocity.x > decexeleration * delta:
+		motion.x -= decexeleration * delta
+	elif velocity.x < -decexeleration * delta:
+		motion.x += decexeleration * delta
+	else:
+		velocity.x = 0
 
-	velocity += delta * motion
+	velocity += motion 
 	velocity.x = clamp(velocity.x,-1,1)
 	velocity.y = clamp(velocity.y,-1,1)
 
@@ -42,3 +47,7 @@ func _process(delta):
 	
 	position.x = clamp(position.x, 0, get_viewport_rect().size.x)
 	position.y = clamp(position.y, 0, get_viewport_rect().size.y)
+
+func _on_TouchScreenButton_pressed():
+	player.stop(true)
+	player.play("FadeToPink", 0, animation_speed)
