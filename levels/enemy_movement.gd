d1a4c543c0ff140a1a057572b3e4e46f8b6105d0
ido <games@ido.wtf>
2022-09-03 22:12:07 +0300

    basic chase script

--- levels/enemy_movement.gd
+++ levels/enemy_movement.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
-export var SPEED = 250
+export var speed = 250
+export var radius = 300
 
-onready var enemy = $"AnimationPlayer"
+onready var animation = $"AnimationPlayer"
 onready var player: = get_node("/root/Node2D/Player")
 
 var velocity
@@ -11,42 +12,26 @@ func _ready():
 	velocity = Vector2(0,0)
 
 func _process(delta):
-	
-	if Input.is_action_just_released("ui_up"):
-		velocity.y = 0
-	if Input.is_action_just_released("ui_down"):
-		velocity.y = 0
-	if Input.is_action_just_released("ui_left"):
-		velocity.x = 0
-	if Input.is_action_just_released("ui_right"):
-		velocity.x = 0
-
 	var motion = Vector2(0,0)
-	if Input.is_action_pressed("ui_up"):
-		motion += Vector2(0,-1)
-	if Input.is_action_pressed("ui_down"):
-		motion += Vector2(0,1)
-	if Input.is_action_pressed("ui_left"):
-		motion += Vector2(-1,0)
-	if Input.is_action_pressed("ui_right"):
-		motion += Vector2(1,0)
+	print(str(get_global_position().distance_to(player.get_global_position())))
+	if get_global_position().distance_to(player.get_global_position()) < radius:
+		if get_global_position().y > player.get_global_position().y:
+			motion += Vector2(0,-1)
+		if get_global_position().y < player.get_global_position().y:
+			motion += Vector2(0,1)
+		if get_global_position().x > player.get_global_position().x:
+			motion += Vector2(-1,0)
+		if get_global_position().x < player.get_global_position().x:
+			motion += Vector2(1,0)
 
-	velocity += delta * motion
+	velocity = motion
 	velocity.x = clamp(velocity.x,-1,1)
 	velocity.y = clamp(velocity.y,-1,1)
 
-#	pos += velocity * SPEED
-
-	move_and_slide(velocity*SPEED)
+	move_and_slide(velocity*speed)
 	if velocity.x == 0 && velocity.y == 0:
-		enemy.play("Idle")
+		animation.play("Idle")
 	else: 
-		enemy.play("Walk")
+		animation.play("Walk")
 		if velocity.x > 0: get_node("Face").flip_h = true 
 		else: get_node("Face").flip_h = false	
-	
-#	var size = get_viewport_rect().size
-#	var pos = get_position()
-#	pos.x = clamp (pos.x,0,size.x)
-#	pos.y = clamp (pos.y,0,size.y)
-#	set_position(pos)
