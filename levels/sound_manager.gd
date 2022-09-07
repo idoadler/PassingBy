fc3d6c9de4696f53cae4e3e83608f6d29b1d7cf3
ido <games@ido.wtf>
2022-09-07 16:56:18 +0300

    added party

--- levels/sound_manager.gd
+++ levels/sound_manager.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export(float) var door_distance = 600
+export(bool) var disable_transition = false
 
 onready var button = $"Button"
 onready var on_sprite = $"Sound_on"
@@ -12,18 +13,18 @@ onready var player: = $"%Player"
 var door_pos
 
 func _ready():
-	if door:
-		door_pos = door.get_global_position()
-	else:
+	if disable_transition:
 		room_music.volume_db = linear2db(1)
 		party_music.volume_db = linear2db(0)
+	else:
+		door_pos = door.get_global_position()
 	button.set_pressed_no_signal(PlayerVariables.muted)
 	_on_Button_toggled(PlayerVariables.muted)
 
 func _process(delta):
 	if PlayerVariables.muted:
 		return
-	if !door:
+	if disable_transition:
 		return
 	var distance = door_pos.distance_to(player.get_global_position())
 	if distance > door_distance:
