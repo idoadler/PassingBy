044cd9ce3e7b30688bb3d5787947e84a9ed8ef54
Ido Adler <games@ido.wtf>
2022-09-10 21:27:02 +0300

    make pinks move constantly

--- levels/pushable_movement.gd
+++ levels/pushable_movement.gd
@@ -6,16 +6,28 @@ export var min_dist = 25
 onready var animation = $"AnimationPlayer"
 
 var start_pos
+var target_pos
+var last_timer = OS.get_ticks_msec()
+var reset_timer = rand_range(100, 800)
+
+func _rand_pos():
+	last_timer = OS.get_ticks_msec()
+	target_pos = start_pos + Vector2(rand_range(-50,50), rand_range(-50,50))
+	reset_timer = rand_range(100, 800)
 
 func _ready():
+	randomize()
 	start_pos = get_global_position()
+	target_pos = start_pos
 
 func _process(delta):
+	if OS.get_ticks_msec() - last_timer > reset_timer:
+		_rand_pos()
 	var motion
 	var cur_pos = get_global_position()
-	var dist = start_pos.distance_to(cur_pos)
+	var dist = target_pos.distance_to(cur_pos)
 	if dist > min_dist:
-		motion = start_pos - cur_pos
+		motion = target_pos - cur_pos
 	else:
 		motion = Vector2(0,0)
 
